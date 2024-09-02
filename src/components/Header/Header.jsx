1e9d14a3185650e2d2b29f1931bf7a04000c9cb8
Maynul <maynul.programmer24@gmail.com>
2024-09-02 09:09:25 -0700

    json data added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+
+function Header(props) {
+    return (
+        <div>
+             <h1 className='text-4xl font-bold'>React Knowledge Cate</h1>
+        </div>
+    );
+}
+
+export default Header;
