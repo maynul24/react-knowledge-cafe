815f1ab0020116f75a8176af91367c983081ef53
Maynul <maynul.programmer24@gmail.com>
2024-09-02 10:10:07 -0700

    set layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
+import profile from '../../assets/images/profile.png'
 
 const Header = () => {
     return (
-        <div>
+        <div className='flex justify-between p-4 mx-4 border-b-2'>
              <h1 className='text-4xl font-bold'>React Knowledge Cate</h1>
+             <img src={profile} alt=""/>
         </div>
     );
 }
