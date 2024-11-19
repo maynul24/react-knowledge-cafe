904d640e2bef53a92dcda763abbbf3529782a8fc
Maynul <maynul.programmer24@gmail.com>
2024-11-20 01:54:10 +0600

    Added Bookmark list when click on the button

--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -1,10 +1,20 @@
+import PropTypes from 'prop-types';
+import Bookmark from "../Bookmark/Bookmark";
+
 const Bookmarks = ({bookmarks}) => {
-    console.log(bookmarks.length);
+    console.log(bookmarks);
     return (
         <div className="md:w-1/3">
             <h2>Bookmarks:{bookmarks.length}</h2>
+            {
+                bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark}></Bookmark>)
+            }
         </div>
     );
 };
+
+Bookmarks.propTypes = {
+    bookmarks: PropTypes.array
+}
 
 export default Bookmarks;
